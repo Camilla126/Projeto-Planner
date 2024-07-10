7cfbf3f9212b756f9b65677f20e4991cb9a8b1fb
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-10 12:21:04 -0300

    Adicionando o moldal

--- src/app.tsx
+++ src/app.tsx
@@ -79,7 +79,11 @@ setIsGuestModalOpen(true)
       </div>
 
       {isGuestModalOpen && (
-        <div className='fixed inset-0 bg-black/60'></div>
+        <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900'>
+            <h2>Selecionar convidados</h2>
+          </div>
+        </div>
       )
       }
     </div>
