70b7c27721c2a20fe2986eb3418dc7f2c7176eb2
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-10 11:03:15 -0300

    Concertando erro

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react'
 import { useState } from 'react'
 
 
@@ -24,12 +24,19 @@ function openGuestsInput () {
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
 
           <div className='flex items-center gap-2 flex-1'><MapPin className='size-5 text-zinc-400' />
-            <input type="text" placeholder="Quem estará na viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" /></div>
+            <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" /></div>
+
+            <div className='flex items-center gap-2'>
+  <Calendar className='size-5 text-zinc-400' />
+  <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" /> </div>
 
           <div className='w-px h-6 bg-zinc-800'></div>
 
          {isGuestsInputOpen ? (
-          <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>Alterar local/data</button>
+          <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+            Alterar local/data
+            <Settings2 className='size-5' />
+            </button>
          ): (
           <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
             <ArrowRight className='size-5' />
@@ -42,11 +49,7 @@ function openGuestsInput () {
         {isGuestsInputOpen && (<div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
 
 <div className='flex items-center gap-2 flex-1'><UserRoundPlus className='size-5 text-zinc-400' />
-  <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" /></div>
-
-<div className='flex items-center gap-2'>
-  <Calendar className='size-5 text-zinc-400' />
-  <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" /> </div>
+  <input type="text" placeholder="Quem estará na viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" /></div>
 
 <div className='w-px h-6 bg-zinc-800'></div>
 
