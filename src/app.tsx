b8e8712d43989643c65c64d1780c80afc2624fd3
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-09 22:23:22 -0300

    Adicionando local/data

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus } from 'lucide-react'
 import { useState } from 'react'
 
 
@@ -28,16 +28,20 @@ function openGuestsInput () {
 
           <div className='w-px h-6 bg-zinc-800'></div>
 
+         {isGuestsInputOpen ? (
+          <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>Alterar local/data</button>
+         ): (
           <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
             <ArrowRight className='size-5' />
             Continuar
           </button>
+         )}
 
         </div>
 
         {isGuestsInputOpen && (<div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
 
-<div className='flex items-center gap-2 flex-1'><MapPin className='size-5 text-zinc-400' />
+<div className='flex items-center gap-2 flex-1'><UserRoundPlus className='size-5 text-zinc-400' />
   <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" /></div>
 
 <div className='flex items-center gap-2'>
@@ -46,9 +50,9 @@ function openGuestsInput () {
 
 <div className='w-px h-6 bg-zinc-800'></div>
 
-<button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+<button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
   <ArrowRight className='size-5' />
-  Continuar
+  Confirmar viagem
 </button>
 
 </div>
