f260c770a2d08e431d34dfed5f056664e43550d2
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-10 21:12:28 -0300

    Excluindo alguns elementos desnecessários

--- src/app.tsx
+++ src/app.tsx
@@ -176,35 +176,20 @@ export function App() {
           <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
             <div className="space-y-2">
               <div className="flex items-center justify-between">
-                <h2 className="font-lg font-semibold">Selecionar convidados</h2>
+                <h2 className="font-lg font-semibold">Confirmar criação da viagem</h2>
                 <button>
                   <X className="size-5 text-zinc-400" onClick={closeGuestsModal} />
                 </button>
               </div>
 
               <p className="text-sm text-zinc-400">
-                Os convidados irão receber e-mails para confirmar a participação na viagem.
+              Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span>  nas datas de <span className="font-semibold text-zinc-100"> 16 a 27 de Agosto de 2024 </span>preencha seus dados abaixo:
               </p>
             </div>
-            
-            <div className="flex flex-wrap gap-2">
-              {emailsToInvite.map(email => {
-                return (
-                  <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-                    <span className="text-zinc-300">{email}</span>
-                    <button type="button">
-                      <X onClick={() => removeEmailFromInvites(email)} className="size-4 text-zinc-400" />
-                    </button>
-                  </div>
-                  )
-                }
-              )}
-            </div>
-            
-            <div className="w-full h-px bg-zinc-800" />
+                   
 
-            <form onSubmit={addNewEmailToInvite} className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-              <div className="px-2 flex items-center flex-1 gap-2">
+            <form onSubmit={addNewEmailToInvite} >
+              <div className="py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 ">
                 <AtSign className="text-zinc-400 size-5" />
                 <input
                   type="email"
