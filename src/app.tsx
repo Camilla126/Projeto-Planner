870524f38549719f8d17f0ff08039cdcc52d9291
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-11 18:11:16 -0300

    Criando navegação do usuário

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,18 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { CreateTripPage } from "./pages/create-trip";
+import { TripDetailsPage } from "./pages/create-trip/trip-details";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <CreateTripPage/>,
   },
 
   {
-    path: "/teste",
-    element: <div>Hello teste!</div>,
+    path: "/trips/:tripId",
+    element: <TripDetailsPage />,
   },
 ]);
 
