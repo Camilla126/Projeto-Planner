9f98dc62059f968171265b017369abed9cd568ee
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-10 21:19:56 -0300

    colocando mais 2 inputs + 1 botão

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, AtSign, Calendar, MapPin, Plus, Settings2, UserRoundPlus, X } from "lucide-react";
+import { ArrowRight, AtSign, Calendar, MapPin, Plus, Settings2, User, UserRoundPlus, X } from "lucide-react";
 import { FormEvent, useState } from "react";
 
 export function App() {
@@ -188,18 +188,28 @@ export function App() {
             </div>
                    
 
-            <form onSubmit={addNewEmailToInvite} >
-              <div className="py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 ">
-                <AtSign className="text-zinc-400 size-5" />
+            <form onSubmit={addNewEmailToInvite} className="space-y-3" >
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 ">
+                <User className="text-zinc-400 size-5" />
                 <input
-                  type="email"
-                  name="email"
-                  placeholder="Digite o email do convidado"
+                  type="text"
+                  name="name"
+                  placeholder="Seu nome completo"
                   className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
                 />
               </div>
 
-              <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 ">
+                <User className="text-zinc-400 size-5" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Seu e-mail pessoal"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Convidar
                 <Plus className="size-5" />
               </button>
