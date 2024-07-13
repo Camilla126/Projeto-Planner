a0f07e11341b6eb0c8e5623770e1363ac53cf366
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-13 20:58:58 -0300

    Layout esquerdo finalizado

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -21,7 +21,7 @@
     </button>
 </div>
                 </div>
-                <main className="flex gap-16">
+                <main className="flex gap-16 px-4">
 <div className="flex-1 space-y-6">
     <div>
         <div className="flex items-center justify-between">
@@ -53,11 +53,23 @@
    <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
    <CircleCheck className="size-5 text-lime-300"/>
    <span className="text-zinc-100">Academia em grupo</span>
-   <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+   <span className="text-zinc-400 text-sm">08:00h</span>
    
    </div>
 </div>
-
+<div className="space-y-2.5">
+    <div className="flex gap-2 items-baseline">
+        <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
+        <span className="text-xs text-zinc-500">Sábado</span>
+    </div>
+   <div className="space-y-2.5"></div>
+   <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+   <CircleCheck className="size-5 text-lime-300"/>
+   <span className="text-zinc-100">Academia em grupo</span>
+   <span className="text-zinc-400 text-sm">08:00h</span>
+   
+   </div>
+</div>
 </div>
 
 </div>
