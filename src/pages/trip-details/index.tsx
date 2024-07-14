f2ec7e3e2eafa41a5b4c69fc10a2393097e8adee
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-13 21:31:30 -0300

    Links estilizados

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -81,12 +81,12 @@
 <div className="space-y-6">
 <h2 className="font-semibold text-xl">Links importantes</h2>
 <div className="space-y-5">
-<div className="flex items-center justify-between">
+<div className="flex items-center justify-between gap-4">
     <div className="space-y-1.5">
         <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
         <span className="block text-xs text-zinc-400 truncate">https://www.airbnb.com.br/rooms/104700011</span>
     </div>
-    <Link2 className="text-zinc-400 size-5"/>
+    <Link2 className="text-zinc-400 size-5 shrink-0"/>
 
 </div>
 </div>
