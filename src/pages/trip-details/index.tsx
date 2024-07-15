7d934a3b11fcc826daf515611d3ecc6992549b2e
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-15 14:32:12 -0300

    Fazendo a segunda separação de componentes

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,5 +1,7 @@
-    import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, UserCog, X } from "lucide-react";
+    import { Calendar, CircleCheck, CircleDashed, MapPin, Plus, Settings2, UserCog, } from "lucide-react";
 import { useState } from "react";
+import { CreateActivityModal } from "./create-activity-modal";
+import { ImportantLinks } from "./important-links";
 
     export function TripDetailsPage() {
 const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
@@ -90,36 +92,8 @@ function closeCreateActivityModal() {
 
 
 <div className="w-80 space-y-6">
-<div className="space-y-6">
-<h2 className="font-semibold text-xl">Links importantes</h2>
-<div className="space-y-5">
-<div className="flex items-center justify-between gap-4">
-    <div className="space-y-1.5">
-        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-        <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/104700011</a>
-    </div>
-    <Link2 className="text-zinc-400 size-5 shrink-0"/>
-</div>
-<div className="flex items-center justify-between gap-4">
-    <div className="space-y-1.5">
-        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-        <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/104700011</a>
-    </div>
-    <Link2 className="text-zinc-400 size-5 shrink-0"/>
+    <ImportantLinks/>
 
-</div>
-
-
-
-
-
-</div>
-
-<button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
-        <Plus className="size-5" />
-         Cadastrar novo link
-    </button>
-</div>
 <div className="w-full h-px bg-zinc-800"></div>
 <div>
 
@@ -159,50 +133,9 @@ function closeCreateActivityModal() {
 
 
 {isCreateActivityModalOpen &&(
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-      <div className="space-y-2">
-        <div className="flex items-center justify-between">
-          <h2 className="font-lg font-semibold">Cadastrar atividade</h2>
-          <button>
-            <X className="size-5 text-zinc-400" onClick={closeCreateActivityModal} />
-          </button>
-        </div>
-    
-        <p className="text-sm text-zinc-400">
-       Todos convidados podem visualizar as atividades.
-        </p>
-      </div>
-    
-      <form className="space-y-3">
-        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-          <Tag className="text-zinc-400 size-5" />
-          <input               
-            name="title"
-            placeholder=" Qual a atividade?"
-            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-          />
-        </div>
-    
-    <div className="flex items-center gap-2">
-    <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-          <Calendar className="text-zinc-400 size-5" />
-          <input
-            type="datetime-local"
-            name="occurs_at"
-            placeholder="Data e horário da atividade"
-            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1 [color-scheme:dark]"
-          />
-        </div>
-
-    </div>
-    
-        <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11  font-medium flex items-center gap-2 hover:bg-lime-400">
-          Salvar atividade
-        </button>
-      </form>
-    </div>
-    </div>
+   <CreateActivityModal 
+   closeCreateActivityModal={closeCreateActivityModal}
+   />
 )}
 
 
