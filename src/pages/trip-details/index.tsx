b4e316678868af1720247ba930daa7165b90ff11
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-15 14:01:16 -0300

    Adicionando um novo input

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,5 @@
-    import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, User, UserCog, X } from "lucide-react";
+    import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, User, UserCog, X } from "lucide-react";
+import { useState } from "react";
 
     export function TripDetailsPage() {
 const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false)
@@ -38,7 +39,7 @@ function closeopenCreateActivityModal() {
         <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
             
-            <button  className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button onClick={openCreateActivityModal}  className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Cadastrar atividade
                 <Plus className="size-5" />
             </button>
@@ -162,20 +163,20 @@ function closeopenCreateActivityModal() {
     <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
       <div className="space-y-2">
         <div className="flex items-center justify-between">
-          <h2 className="font-lg font-semibold">Confirmar criação da viagem</h2>
+          <h2 className="font-lg font-semibold">Cadastrar atividade</h2>
           <button>
             <X className="size-5 text-zinc-400" onClick={closeopenCreateActivityModal} />
           </button>
         </div>
     
         <p className="text-sm text-zinc-400">
-        Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span > nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+       Todos convidados podem visualizar as atividades.
         </p>
       </div>
     
       <form className="space-y-3">
         <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-          <User className="text-zinc-400 size-5" />
+          <Tag className="text-zinc-400 size-5" />
           <input               
             name="title"
             placeholder=" Qual a atividade?"
@@ -183,7 +184,8 @@ function closeopenCreateActivityModal() {
           />
         </div>
     
-        <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+    <div className="flex items-center gap-2">
+    <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
           <User className="text-zinc-400 size-5" />
           <input
             type="email"
@@ -192,6 +194,7 @@ function closeopenCreateActivityModal() {
             className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
           />
         </div>
+    </div>
     
         <button type="submit" className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11  font-medium flex items-center gap-2 hover:bg-lime-400">
           Salvar atividade
