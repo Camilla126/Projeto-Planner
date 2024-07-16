de3237cf128e2a2af0062833035bdfb75e12d823
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-16 15:39:54 -0300

    Adicionando variaveis ptimaty e secondary

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,7 +3,21 @@ import { ComponentProps, ReactNode } from "react";
 import {tv} from 'tailwind-variants'
 
 const buttonVariants = tv({
-    base: 'rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'
+    base: 'rounded-lg px-5 py-2 font-medium flex items-center gap-2',
+
+variants: {
+variant: {
+    primary: 'bg-lime-300 text-lime-950 hover:bg-lime-400',
+    secondary: 'bg-zinc-800 text-zinc-200 hover:bg-zinc-700',
+}
+
+},
+
+    
+defaultVariants: {
+    variant: 'primary',
+}
+
 })
 
 
@@ -13,7 +27,7 @@ interface ButtonProps extends ComponentProps<'button'> {
 
 export function Button({children, ...props}: ButtonProps){
     return (
-        <button {...props} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+        <button {...props} className={buttonVariants()}>
         {children}
     </button>
     )
