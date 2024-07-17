74542b73b85578f3925bf23f342b56ec2487502a
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-17 19:48:36 -0300

    Copiando modal para destination

--- src/pages/steps/destination-and-date-step.tsx
+++ src/pages/steps/destination-and-date-step.tsx
@@ -40,6 +40,25 @@ function openDatePicker(){
           </span>
         </button>
 
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+<div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+  <div className="space-y-2">
+    <div className="flex items-center justify-between">
+      <h2 className="font-lg font-semibold">Confirmar criação da viagem</h2>
+      <button>
+        <X className="size-5 text-zinc-400" onClick={closeConfirmTripModal} />
+      </button>
+    </div>
+
+    <p className="text-sm text-zinc-400">
+    Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span > nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+    </p>
+  </div>
+
+ <h1>hellooooooooooo</h1>
+</div>
+</div>
+
         <div className="w-px h-6 bg-zinc-800" />
 
         {isGuestsInputOpen ? (
