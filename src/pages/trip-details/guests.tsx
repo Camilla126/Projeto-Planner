7f679ab51df64dd63544b1d146c8d34dcb3762a9
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-21 21:48:21 -0300

    aplicando as fuções do map

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -26,10 +26,10 @@ export function Guests(){
 <div className="space-y-5">
 {participants.map(participant => {
     return (
-        <div className="flex items-center justify-between gap-4">
+        <div key={participant.id} className="flex items-center justify-between gap-4">
     <div className="space-y-1.5">
-        <span className="block font-medium text-zinc-100">Camilla carvalho</span>
-        <span className="block text-sm text-zinc-400 truncate ">camilla.carvalho@gmail.com</span>
+        <span className="block font-medium text-zinc-100">{participant.name}</span>
+        <span className="block text-sm text-zinc-400 truncate ">{participant.email}</span>
     </div>
     <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
 </div>
