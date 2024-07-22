006ad545d573a619d30d313afbe461115109f4e7
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-21 21:20:10 -0300

    corrigindo bug

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -25,9 +25,9 @@ api.get(`/trips/${tripId}`).then(response => setTrip(response.data.trip))
 }, [tripId])
 
 
-const displayedDate = trip
- ? format(trip.starts_at, "d'  de 'LLL").concat(' até ').concat(format(trip.ends_at, "d'  de 'LLL"))
-    : null
+const displayedDate = trip ? format(trip.starts_at, "d' de 'LLL").concat(' até ').concat(format(trip.ends_at, "d' de 'LLL"))
+: null
+
 
     return (
 
@@ -40,7 +40,7 @@ const displayedDate = trip
             <div className="flex items-center gap-5">
                 <div className="flex items-center gap-2">
                     <Calendar className="size-5 text-zinc-400" />
-                    <span className=" text-zinc-100"> {displayedDate}  </span>
+                    <span className=" text-zinc-100"> {displayedDate} </span>
                 </div>
                 <div className="w-px h-6 bg-zinc-800" />
 
