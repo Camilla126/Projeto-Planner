c333fdaf722ee2a9a6dbd08bba4412dd702baaf7
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-21 21:08:33 -0300

    fazendo validação na função de data

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -3,6 +3,7 @@ import { Button } from "../../components/button";
 import { useParams } from "react-router-dom";
 import { api } from "../../lib/axios";
 import { useEffect, useState } from "react";
+import { format } from "date-fns";
 
 interface Trip {
 id: string
@@ -23,6 +24,11 @@ api.get(`/trips/${tripId}`).then(response => setTrip(response.data.trip))
 
 }, [tripId])
 
+
+const displayedDate = trip
+ ? format(trip.starts_at, "d'  de 'LLL").concat(' até ').concat(format(trip.ends_at, "d'  de 'LLL"))
+    : null
+
     return (
 
         <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -34,7 +40,7 @@ api.get(`/trips/${tripId}`).then(response => setTrip(response.data.trip))
             <div className="flex items-center gap-5">
                 <div className="flex items-center gap-2">
                     <Calendar className="size-5 text-zinc-400" />
-                    <span className=" text-zinc-100"> 17 a 23 de Agosto  </span>
+                    <span className=" text-zinc-100"> {displayedDate}  </span>
                 </div>
                 <div className="w-px h-6 bg-zinc-800" />
 
