df7098d4176615c9829a5ed37cd4e5ec9579be0b
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-21 21:45:44 -0300

    criando map

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -2,7 +2,7 @@ import { CircleDashed, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
 import { useParams } from "react-router-dom";
 import { api } from "../../lib/axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface Participant {
     id: string
@@ -13,10 +13,10 @@ interface Participant {
 
 export function Guests(){
     const { tripId } = useParams()
-    const [trip, setTrip] = useState<Trip | undefined>()
+    const [participants, setParticipants] = useState<Participant[]>([])
     
     useEffect(() => {
-    api.get(`/trips/${tripId}/participants`).then(response => setTrip(response.data.participants))
+    api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
     }, [tripId])
     
 
@@ -24,21 +24,18 @@ export function Guests(){
         <div className="space-y-6">
 <h2 className="font-semibold text-xl">Convidados</h2>
 <div className="space-y-5">
-<div className="flex items-center justify-between gap-4">
+{participants.map(participant => {
+    return (
+        <div className="flex items-center justify-between gap-4">
     <div className="space-y-1.5">
         <span className="block font-medium text-zinc-100">Camilla carvalho</span>
         <span className="block text-sm text-zinc-400 truncate ">camilla.carvalho@gmail.com</span>
     </div>
     <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
 </div>
-<div className="flex items-center justify-between gap-4">
-    <div className="space-y-1.5">
-        <span className="block font-medium text-zinc-100">Breno</span>
-        <span className="block text-sm text-zinc-400 truncate ">breno.nascimento@gmail.com</span>
-    </div>
-    <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+    )
+})}
 
-</div>
 
 </div>
 
